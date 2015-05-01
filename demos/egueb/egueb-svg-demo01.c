624a0b619ac7953cb0c73a84746dcea260c5cfa1
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-05-02 00:47:22 +0200

    Start adding demos

---
+++ demos/egueb/egueb-svg-demo01.c
@@ -0,0 +1,168 @@
+/* To compile:
+ * gcc -Wall egueb-svg-demo01.c -o egueb-svg-demo01 `pkg-config --cflags --libs egueb-smil egueb-svg egueb-dom ecore efl-egueb`
+ * Idea taken from http://bl.ocks.org/mbostock/1062544
+ */
+#include "Efl_Egueb.h"
+#include "Egueb_Smil.h"
+#include "Egueb_Svg.h"
+#include "Ecore.h"
+
+int i = 0;
+
+/* once the animation stops we need to remove the element from the tree
+ * and finally destroy it
+ */
+static void _on_anim_end(Egueb_Dom_Event *ev, void *data)
+{
+	Egueb_Dom_Node *anim;
+	Egueb_Dom_Node *svg;
+	Egueb_Dom_Node *parent;
+
+	anim = egueb_dom_event_target_get(ev);
+	parent = egueb_dom_node_parent_get(anim);
+	svg = egueb_dom_event_target_current_get(ev);
+	egueb_dom_node_child_remove(svg, parent, NULL);
+	egueb_dom_node_unref(parent);
+}
+
+static void _on_rect_mouse_move(Egueb_Dom_Event *ev, void *data)
+{
+	Egueb_Dom_Node *circle;
+	Egueb_Dom_Node *parent;
+	Egueb_Dom_Node *anim;
+	Egueb_Svg_Length cx, cy, r, sw;
+	Egueb_Svg_Paint paint;
+	int mx, my;
+
+	mx = egueb_dom_event_mouse_client_x_get(ev);
+	my = egueb_dom_event_mouse_client_y_get(ev);
+	/* Create a new circle on x,y position */
+	circle = egueb_svg_element_circle_new();
+	egueb_svg_length_set(&cx, mx, EGUEB_SVG_LENGTH_UNIT_PX);
+	egueb_svg_length_set(&cy, my, EGUEB_SVG_LENGTH_UNIT_PX);
+	egueb_svg_length_set(&r, 0.1, EGUEB_SVG_LENGTH_UNIT_PX);
+	egueb_svg_length_set(&sw, 2.5, EGUEB_SVG_LENGTH_UNIT_PX);
+	egueb_svg_element_circle_cx_set(circle, &cx);
+	egueb_svg_element_circle_cy_set(circle, &cy);
+	egueb_svg_element_circle_r_set(circle, &r);
+	paint.type = EGUEB_SVG_PAINT_TYPE_NONE;
+	egueb_svg_element_fill_set(circle, &paint);
+	paint.type = EGUEB_SVG_PAINT_TYPE_COLOR;
+	/* TODO we should do HLS -> RGB */
+	i = (i + 1) % 255;
+	egueb_svg_color_components_from(&paint.color, i, 0, 0);
+	egueb_svg_element_stroke_set(circle, &paint);
+	egueb_svg_element_stroke_width_set(circle, &sw);
+	/* set the color based on the previous color */
+	/* add an animation of the stroke color */
+	{
+		Egueb_Dom_String *attr;
+		Egueb_Dom_String *value;
+
+		anim = egueb_smil_animate_new();
+		attr = egueb_dom_string_new_with_static_string("attributeName");
+		value = egueb_dom_string_new_with_static_string("opacity");
+		egueb_dom_element_attribute_set(anim, attr, value, NULL);
+		egueb_dom_string_unref(attr);
+		egueb_dom_string_unref(value);
+
+		attr = egueb_dom_string_new_with_static_string("from");
+		value = egueb_dom_string_new_with_static_string("1");
+		egueb_dom_element_attribute_set(anim, attr, value, NULL);
+		egueb_dom_string_unref(attr);
+		egueb_dom_string_unref(value);
+
+		attr = egueb_dom_string_new_with_static_string("to");
+		value = egueb_dom_string_new_with_static_string("0");
+		egueb_dom_element_attribute_set(anim, attr, value, NULL);
+		egueb_dom_string_unref(attr);
+		egueb_dom_string_unref(value);
+
+		attr = egueb_dom_string_new_with_static_string("dur");
+		value = egueb_dom_string_new_with_static_string("0.5s");
+		egueb_dom_element_attribute_set(anim, attr, value, NULL);
+		egueb_dom_string_unref(attr);
+		egueb_dom_string_unref(value);
+
+		egueb_dom_node_child_append(circle, anim, NULL);
+	}
+	{
+		Egueb_Dom_String *attr;
+		Egueb_Dom_String *value;
+
+		anim = egueb_smil_animate_new();
+		attr = egueb_dom_string_new_with_static_string("attributeName");
+		value = egueb_dom_string_new_with_static_string("r");
+		egueb_dom_element_attribute_set(anim, attr, value, NULL);
+		egueb_dom_string_unref(attr);
+		egueb_dom_string_unref(value);
+
+		attr = egueb_dom_string_new_with_static_string("from");
+		value = egueb_dom_string_new_with_static_string("0.1");
+		egueb_dom_element_attribute_set(anim, attr, value, NULL);
+		egueb_dom_string_unref(attr);
+		egueb_dom_string_unref(value);
+
+		attr = egueb_dom_string_new_with_static_string("to");
+		value = egueb_dom_string_new_with_static_string("100");
+		egueb_dom_element_attribute_set(anim, attr, value, NULL);
+		egueb_dom_string_unref(attr);
+		egueb_dom_string_unref(value);
+
+		attr = egueb_dom_string_new_with_static_string("dur");
+		value = egueb_dom_string_new_with_static_string("0.5s");
+		egueb_dom_element_attribute_set(anim, attr, value, NULL);
+		egueb_dom_string_unref(attr);
+		egueb_dom_string_unref(value);
+
+		egueb_dom_node_child_append(circle, anim, NULL);
+	}
+	/* add the circle after the rect */
+	parent = egueb_dom_event_target_current_get(ev);
+	egueb_dom_node_child_append(parent, circle, NULL);
+	egueb_dom_node_unref(parent);
+}
+
+int main(void)
+{
+	Efl_Egueb_Window *w;
+	Egueb_Dom_Node *doc;
+	Egueb_Dom_Node *svg;
+	Egueb_Dom_Node *rect;
+	Egueb_Svg_Length width, height;
+	Egueb_Svg_Paint paint;
+
+	if (!efl_egueb_init())
+		return -1;
+
+	doc = egueb_svg_document_new();
+	svg = egueb_svg_element_svg_new();
+	/* create a window of size 960x500 */
+	egueb_svg_length_set(&width, 960, EGUEB_SVG_LENGTH_UNIT_PX);
+	egueb_svg_length_set(&height, 500, EGUEB_SVG_LENGTH_UNIT_PX);
+	egueb_svg_element_svg_width_set_simple(svg, &width);
+	egueb_svg_element_svg_height_set_simple(svg, &height);
+	/* add our rectangle */
+	rect = egueb_svg_element_rect_new();
+	paint.type = EGUEB_SVG_PAINT_TYPE_COLOR;
+	egueb_svg_color_components_from(&paint.color, 0x22, 0x22, 0x22);
+	egueb_svg_element_fill_set(rect, &paint);
+	egueb_svg_element_rect_width_set_simple(rect, &width);
+	egueb_svg_element_rect_height_set_simple(rect, &height);
+	/* register the mousemove event */
+	egueb_dom_node_event_listener_add(svg, EGUEB_DOM_EVENT_MOUSE_MOVE,
+			_on_rect_mouse_move, EINA_FALSE, NULL);
+	/* register the anim end event */
+	egueb_dom_node_event_listener_add(svg, EGUEB_SMIL_EVENT_END,
+			_on_anim_end, EINA_TRUE, NULL);
+	egueb_dom_node_child_append(svg, rect, NULL);
+	/* append it as our own topmost element */	
+	egueb_dom_node_child_append(doc, svg, NULL);
+	
+	w = efl_egueb_window_auto_new(doc, 0, 0, 960, 500);
+	ecore_main_loop_begin();
+
+	efl_egueb_window_free(w);
+	efl_egueb_shutdown();
+	return 0;
+}
