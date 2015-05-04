cbc2236abff68c2d058eddd895af839dcedea998
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-05-05 01:07:51 +0200

    Add the honeycomb pattern

--- demos/egueb/egueb-svg-demo02.c
+++ demos/egueb/egueb-svg-demo02.c
@@ -17,6 +17,7 @@ int main(void)
 	Egueb_Dom_Node *patt;
 	Egueb_Dom_Node *path;
 	Egueb_Svg_Length width, height;
+	Egueb_Svg_Length sw;
 	Egueb_Svg_Paint paint;
 
 	if (!efl_egueb_init())
@@ -27,14 +28,44 @@ int main(void)
 	/* create our SVG */
 	egueb_svg_length_set(&width, 100, EGUEB_SVG_LENGTH_UNIT_PERCENT);
 	egueb_svg_length_set(&height, 100, EGUEB_SVG_LENGTH_UNIT_PERCENT);
+	egueb_svg_element_svg_width_set_simple(svg, &width);
+	egueb_svg_element_svg_height_set_simple(svg, &height);
 	/* create the background pattern */
 	patt = egueb_svg_element_pattern_new();
-	egueb_svg_element_id_set(patt, egueb_dom_string_new_with_static_string("patt"));
+	egueb_svg_element_id_set(patt, egueb_dom_string_new_with_static_string("pattern0"));
+	egueb_svg_length_set(&width, 28, EGUEB_SVG_LENGTH_UNIT_PX);
+	egueb_svg_length_set(&height, 50, EGUEB_SVG_LENGTH_UNIT_PX);
+	egueb_svg_element_pattern_width_set(patt, &width);
+	egueb_svg_element_pattern_height_set(patt, &height);
+	egueb_svg_element_pattern_pattern_units_set(patt, EGUEB_SVG_REFERENCEABLE_UNITS_USER_SPACE_ON_USE);
+	/* create the path pattern */
 	path = egueb_svg_element_path_new();
+	{
+		Egueb_Dom_String *val;
+		Egueb_Dom_String *attr;
+
+		val = egueb_dom_string_new_with_static_string("M14 33L0 25L0 8L14 0L28 8L28 25L14 33L14 50");
+		attr = egueb_dom_string_new_with_static_string("d");
+		egueb_dom_element_attribute_set(path, attr, val, NULL);
+	}
+	paint.type = EGUEB_SVG_PAINT_TYPE_NONE;
+	egueb_svg_element_fill_set(path, &paint);
+	paint.type = EGUEB_SVG_PAINT_TYPE_COLOR;
+	egueb_svg_color_components_from(&paint.color, 0, 0, 0);
+	egueb_svg_element_stroke_set(path, &paint);
+	egueb_svg_length_set(&sw, 0.5, EGUEB_SVG_LENGTH_UNIT_PX);
+	egueb_svg_element_stroke_width_set(path, &sw);
+	egueb_dom_node_child_append(patt, path, NULL);
 	egueb_dom_node_child_append(svg, patt, NULL);
-	
 	/* create the background */
 	rect = egueb_svg_element_rect_new();
+	paint.type = EGUEB_SVG_PAINT_TYPE_SERVER;
+	paint.uri = egueb_dom_string_new_with_static_string("url(#pattern0)");
+	egueb_svg_element_fill_set(rect, &paint);
+	egueb_svg_length_set(&width, 100, EGUEB_SVG_LENGTH_UNIT_PERCENT);
+	egueb_svg_length_set(&height, 100, EGUEB_SVG_LENGTH_UNIT_PERCENT);
+	egueb_svg_element_rect_width_set_simple(rect, &width);
+	egueb_svg_element_rect_height_set_simple(rect, &height);
 	egueb_dom_node_child_append(svg, rect, NULL);
 	/* append it as our own topmost element */	
 	egueb_dom_node_child_append(doc, svg, NULL);
