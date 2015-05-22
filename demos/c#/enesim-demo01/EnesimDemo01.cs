321ed86fa1f9a1df43b1599ca09f66db78f1a260
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-05-22 20:23:56 +0200

    Add the first c# demo

---
+++ demos/c#/enesim-demo01/EnesimDemo01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Enesim;
+using Eina;
+
+namespace enesimdemo01
+{
+	public class EnesimDemo01
+	{
+		static bool DamageCb (Enesim.Renderer r2, Eina.Rectangle area, bool past, IntPtr data)
+		{
+			Console.WriteLine ("damage received " + area.X + " " + area.Y + " " + area.W + " " + area.H);
+			return true;
+		}
+
+		public EnesimDemo01 ()
+		{
+
+		}
+
+		static void Main() {
+			Enesim.Main.Init ();
+			Enesim.Renderer.Circle r = new Enesim.Renderer.Circle ();
+			r.Radius = 100;
+			r.GetDamages (DamageCb, IntPtr.Zero);
+		}
+	}
+}
+
