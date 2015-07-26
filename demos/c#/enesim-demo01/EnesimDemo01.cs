009e7b0e0db8f5bde2d9c7955e9ff1e1ba72719d
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-07-26 18:03:04 +0200

    Improve the demo to test more binding features

--- demos/c#/enesim-demo01/EnesimDemo01.cs
+++ demos/c#/enesim-demo01/EnesimDemo01.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using Enesim;
+using Ender;
 using Eina;
 
 namespace enesimdemo01
 {
 	public class EnesimDemo01
 	{
-		static bool DamageCb (Enesim.Renderer r2, Eina.Rectangle area, bool past, IntPtr data)
+		static bool DamageCb (Renderer r, Eina.Rectangle area, bool past, IntPtr data)
 		{
+			// Check downcast
+			if (r is RendererCircle) {
+				Console.WriteLine ("is circle");
+			}
+
+			// Check struct
 			Console.WriteLine ("damage received " + area.X + " " + area.Y + " " + area.W + " " + area.H);
 			return true;
 		}
@@ -18,10 +25,32 @@ namespace enesimdemo01
 		}
 
 		static void Main() {
+			// Initialize ender to support downcasting correctly
+			Ender.Main.Init ();
+			// Initialize enesim
 			Enesim.Main.Init ();
-			Enesim.Renderer.Circle r = new Enesim.Renderer.Circle ();
+
+			RendererCircle r = new RendererCircle ();
+			// Test properties
 			r.Radius = 100;
+			r.X = 160;
+			r.Y = 120;
+			// Test parent properties
+			r.StrokeColor = Color.ComponentsFrom (0xff, 0x00, 0xff, 0x00);
+			r.StrokeWeight = 10;
+			r.FillColor = Color.ComponentsFrom (0xff, 0xff, 0x00, 0x00);
+			// Test enum flags
+			r.DrawMode = (int)(RendererShapeDrawMode.Fill | RendererShapeDrawMode.Stroke);
+
+			// Test callbacks/delegates
 			r.GetDamages (DamageCb, IntPtr.Zero);
+
+			// Draw it
+			Log log;
+			Eina.Error err;
+			Surface s = new Surface (Format.Enum.Argb8888, 320, 240);
+			r.Draw (s, Rop.Fill, null, 0, 0, out log);
+			ImageFile.Save ("out.png", s.GetBuffer (), null, out err);
 		}
 	}
 }
