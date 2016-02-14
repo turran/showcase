983ace8d22b4123a720fea4fcdd0b5fb83df298d
Jorge Zapata <jorgeluis.zapata@gmail.com>
2016-02-14 21:23:26 +0100

    Add an egueb demo based on gegueb bindings

---
+++ demos/c#/egueb-demo02/EguebDemo02.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.IO;
+using Egueb.Dom;
+using Enesim;
+using Gegueb;
+
+
+namespace eguebdemo02
+{
+	public class EguebDemo02
+	{
+		static Enesim.Stream LoadResource()
+		{
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			string resourceName = "eguebdemo02.Resources.EguebDemo02.svg";
+			System.IO.Stream stream = assembly.GetManifestResourceStream (resourceName);
+			StreamReader reader = new StreamReader (stream);
+			string result = reader.ReadToEnd();
+			IntPtr rawString = Marshal.StringToHGlobalAnsi (result);
+			Enesim.Stream s = new Enesim.StreamBuffer (rawString, (IntPtr)result.Length, Marshal.FreeHGlobal);
+			return s;
+		}
+
+		static void Main()
+		{
+			Document doc;
+			Application.Init ();
+
+			Parser.Parse (LoadResource (), out doc);
+			if (doc == null) {
+				Console.WriteLine ("No doc");
+				return;
+			}
+
+			Window win = Application.New (doc, 0, 0, -1, -1);
+			Application.Run ();
+			win.CloseNotify ();
+		}
+	}
+}
+
