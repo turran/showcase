8faa2fc3ad45b2d8e2bcf6ce13107b2a0cbb0e98
Jorge Zapata <jorgeluis.zapata@gmail.com>
2016-02-21 02:19:49 +0100

    Add the gegueb widget example

---
+++ demos/c#/egueb-demo03/MainWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Gtk;
+using Egueb.Dom;
+using Gegueb;
+using Enesim;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.IO;
+
+public partial class MainWindow: Gtk.Window
+{
+	Enesim.Stream LoadResource()
+	{
+		Assembly assembly = Assembly.GetExecutingAssembly();
+		string resourceName = "eguebdemo03.Resources.EguebDemo03.svg";
+		System.IO.Stream stream = assembly.GetManifestResourceStream (resourceName);
+		StreamReader reader = new StreamReader (stream);
+		string result = reader.ReadToEnd();
+		IntPtr rawString = Marshal.StringToHGlobalAnsi (result);
+		Enesim.Stream s = new Enesim.StreamBuffer (rawString, (IntPtr)result.Length, Marshal.FreeHGlobal);
+		return s;
+	}
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Document doc = null;
+
+		Build ();
+
+		Gegueb.Widget w = new Gegueb.Widget ();
+		Parser.Parse (LoadResource (), out doc);
+		w.Document = doc;
+		vbox1.Add (w);
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Gtk.Application.Quit ();
+		a.RetVal = true;
+	}
+}
---
+++ demos/c#/egueb-demo03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+using Egueb.Dom;
+
+namespace eguebdemo03
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Egueb.Dom.Main.Init ();
+			Ender.Main.Init ();
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.ShowAll ();
+			Application.Run ();
+		}
+	}
+}
